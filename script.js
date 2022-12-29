a716db0bd364f638aa8029d32109a91f6bdbceda
Fer Porras <ferpm09@gmail.com>
2022-12-28 23:26:54 -0600

    Basic Operators

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // LECTURE: Values and Variables
 const country = 'Costa Rica'; 
 const continent = 'America';
-const population = 5;
+let population = 5;
 
 console.log(country);
 console.log(continent);
@@ -17,4 +17,13 @@ console.log(typeof country)
 console.log(typeof language)
 
 // LECTURE: let, const and var
-language = 'Spanish';
+language = 'spanish';
+
+// LECTURE: Basic Operators
+console.log(population / 2);
+population++;
+console.log(population)
+console.log(population > 6)
+console.log(population < 33)
+const description = `${country} is in ${continent}, and it's ${population} million people speak ${language}.`
+console.log(description)
