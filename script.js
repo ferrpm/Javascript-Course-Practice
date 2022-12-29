7414671d959720146577cf08f8432fb44d754fa8
Fer Porras <ferpm09@gmail.com>
2022-12-28 23:14:59 -0600

    let, const and var

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ console.log(continent);
 console.log(population);
 
 // LECTURE: Data Types
-let isIsland = false
+const isIsland = false
 let language 
 
 console.log(typeof isIsland)
 console.log(typeof population)
 console.log(typeof country)
 console.log(typeof language)
+
+// LECTURE: let, const and var
+language = 'Spanish';
