8eebd6b1ef272e0ba41ad14da840f13501ecffc0
Fer Porras <ferpm09@gmail.com>
2022-12-28 23:13:10 -0600

    Data Types

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const population = 5;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// LECTURE: Data Types
+let isIsland = false
+let language 
+
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
