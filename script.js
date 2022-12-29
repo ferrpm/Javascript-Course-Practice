a40457d4ecd18fc6db4360b526c325ca281f5736
Fer Porras <ferpm09@gmail.com>
2022-12-28 23:55:55 -0600

    The switch Statement

--- script.js
+++ script.js
@@ -58,3 +58,23 @@ if ((language === 'english') && (population < 50) && !isIsland) {
 } else {
     console.log(`${country} does not meet your criteria :(`)
 }
+
+// LECTURE: The switch Statement
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+    case 'arabic':
+        console.log('5th most spoken language');
+    default:
+        console.log('Great language too :D!');
+}
