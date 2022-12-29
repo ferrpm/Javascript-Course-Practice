a0c370ae83a9c78d2e7058a2b809cce9ae16ca9b
Fer Porras <ferpm09@gmail.com>
2022-12-28 23:10:37 -0600

    Values and Variables

---
+++ script.js
@@ -0,0 +1,8 @@
+// LECTURE: Values and Variables
+const country = 'Costa Rica'; 
+const continent = 'America';
+const population = 5;
+
+console.log(country);
+console.log(continent);
+console.log(population);
