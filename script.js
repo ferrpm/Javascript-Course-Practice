6242135ea12aacc8ccaa68c44e7e181988c0c28d
Fer Porras <ferpm09@gmail.com>
2023-01-03 22:23:21 -0600

    Introduction to Arrays

--- script.js
+++ script.js
@@ -117,3 +117,14 @@ const describePopulation = function (country, population) {
 }
 
 describePopulation('Costa Rica', 5);
+
+// LECTURE: Introduction to Arrays
+const populations = [10, 1441, 332, 83];
+console.log(populations.length === 4);
+const percentages = [
+    percentageOfWorld1(populations[0]),
+    percentageOfWorld1(populations[1]),
+    percentageOfWorld1(populations[2]),
+    percentageOfWorld1(populations[3])
+];
+console.log(percentages);
