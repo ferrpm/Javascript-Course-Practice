12e73cc659b962c94ec827f927a841f3a204a53b
Fer Porras <ferpm09@gmail.com>
2023-01-03 23:24:11 -0600

    Iteration: The for Loop

--- script.js
+++ script.js
@@ -183,3 +183,8 @@ const myCountry2 = {
 myCountry2.describe();
 myCountry2.checkIsland();
 console.log(myCountry2);
+
+// LECTURE: Iteration: The for Loop
+for (let voter = 1; voter <= 50; voter++) {
+    console.log(`Voter number ${voter} is currently voting.`);
+};
